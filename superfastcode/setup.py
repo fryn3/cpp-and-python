3bbadd3f29c8d0d594e5a65da05faa9d65e01a10
Мгер <fryn3@mail.ru>
2017-06-10 21:05:48 +0300

    Дошел до пункта "pip install ." -- и она выполняется с ошибкой! Processing c:\users\q\documents\visual studio 2017\projects\pywithcpp\superfastcode Installing collected packages: superfastcode   Running setup.py install for superfastcode ... error Exception: Traceback (most recent call last):   File "c:\program files\python36\lib\site-packages\pip\compat\__init__.py", line 73, in console_to_str     return s.decode(sys.__stdout__.encoding) UnicodeDecodeError: 'utf-8' codec can't decode byte 0xed in position 30: invalid continuation byte
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "c:\program files\python36\lib\site-packages\pip\basecommand.py", line 215, in main
        status = self.run(options, args)
      File "c:\program files\python36\lib\site-packages\pip\commands\install.py", line 342, in run
        prefix=options.prefix_path,
      File "c:\program files\python36\lib\site-packages\pip\req\req_set.py", line 784, in install
        **kwargs
      File "c:\program files\python36\lib\site-packages\pip\req\req_install.py", line 878, in install
        spinner=spinner,
      File "c:\program files\python36\lib\site-packages\pip\utils\__init__.py", line 676, in call_subprocess
        line = console_to_str(proc.stdout.readline())
      File "c:\program files\python36\lib\site-packages\pip\compat\__init__.py", line 75, in console_to_str
        return s.decode('utf_8')
    UnicodeDecodeError: 'utf-8' codec can't decode byte 0xed in position 30: invalid continuation byte
    Я пробовал и setup.py закодировать в utf-8 и module.cpp тоже перевел в utf-8. Не помогло. Что дальше делать?

---
+++ superfastcode/setup.py
@@ -0,0 +1,8 @@
+﻿from distutils.core import setup, Extension, DEBUG
+
+sfc_module = Extension('superfastcode', sources = ['module.cpp'])
+
+setup(name = 'superfastcode', version = '1.0',
+    description = 'Python Package with superfastcode C++ Extension',
+    ext_modules = [sfc_module]
+    )
